42c89760a8981d89a7bc4ad73a581f1d61285aad
FIGUEIRAS Jossua <105790280+Mandicore@users.noreply.github.com>
2023-05-13 11:14:37 +0200

    Ajoutez des fichiers projet.

---
+++ Gestion du temps cse axe system .net 5.0/Form1.cs
@@ -0,0 +1,99 @@
+﻿using projet_gestion_temps_cse_axe_system;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gestion_du_temps_cse_axe_system_.net_5._0
+{
+    public partial class Form1 : Form
+    {
+        private Color background = Color.FromArgb(24, 30, 42);
+        private Color foreground = Color.FromArgb(166, 154, 121);
+
+        private bool panelOpen = false;
+        private Panel panel = new Panel();
+        private Button burgerButton = new Button();
+
+        public List<Personnes> personnes = new List<Personnes>();
+        public Form1()
+        {
+            InitializeComponent();
+
+            int Width = ScreenSetings.GetWidthScreen(this);
+            int Height = ScreenSetings.GetHeightScreen(this);
+
+
+            //form style
+            this.WindowState = FormWindowState.Maximized;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.BackColor = background;
+            this.MaximumSize = Screen.PrimaryScreen.WorkingArea.Size;
+            this.MinimumSize = Screen.PrimaryScreen.WorkingArea.Size;
+
+
+
+            //Home Text (date)
+            Label HomeText = Styles.HomeText(background, foreground, Width, 100, this);
+            this.Controls.Add(HomeText);
+
+            // Menu burger
+            burgerButton = Styles.CreateButton(background, foreground);
+            burgerButton.Click += new EventHandler(burgerButton_Click);
+            this.Controls.Add(burgerButton);
+        }
+        private void burgerButton_Click(object sender, EventArgs e)
+        {
+            if (!panelOpen)
+            {
+                panel = Styles.CreatePanel();
+                this.Controls.Add(panel);
+                burgerButton.BackColor = Color.Gray;
+                burgerButton.FlatAppearance.BorderColor = Color.Gray;
+                burgerButton.ForeColor = Color.Black;
+
+                //personnes = GetListPersonnes.GetListPersonneFromJson();
+
+                if (personnes != null)
+                {
+                    foreach (Personnes personne in personnes)
+                    {
+                        Button buttonPersonne = Styles.CreateButtonMenu(Color.Gray, foreground, personne.name);
+                        panel.Controls.Add(buttonPersonne);
+                    }
+
+                    Button add = Styles.CreateButtonAdd(foreground, Color.Gray, "Ajouter ...");
+                    add.Click += new EventHandler(add_Click);
+                    panel.Controls.Add(add);
+                    panelOpen = true;
+                }
+
+
+            }
+            else
+            {
+                this.Controls.Remove(panel);
+                burgerButton.BackColor = background;
+                burgerButton.FlatAppearance.BorderColor = background;
+                burgerButton.ForeColor = foreground;
+                panelOpen = false;
+
+            }
+        }
+        private void add_Click(object sender, EventArgs e)
+        {
+            Personnes Jossua = new Personnes("FIGUEIRAS", "Jossua", true);
+            personnes.Add(Jossua);
+            ImportJsonFromFile.Send(personnes);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Gestion du temps cse axe system .net 5.0/Program.cs
@@ -0,0 +1,154 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using projet_gestion_temps_cse_axe_system;
+
+namespace Gestion_du_temps_cse_axe_system_.net_5._0
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+    public static class Styles
+    {
+        public static Panel CreatePanel()
+        {
+            Panel menuPanel = new Panel();
+            menuPanel.BackColor = Color.Gray;
+            menuPanel.Dock = DockStyle.Left;
+            menuPanel.Width = 300;
+            return menuPanel;
+        }
+        public static Label HomeText(Color backColor, Color color, int sizeX, int sizeY, Form form)
+        {
+            Label label = new Label();
+            label.Text = GetDate();
+            label.Size = new Size(sizeX - 30, sizeY);
+            label.BackColor = backColor;
+            label.ForeColor = color;
+            label.Font = new Font("Arial", 30, FontStyle.Italic);
+            label.TextAlign = ContentAlignment.MiddleCenter;
+
+            int LocationX = (15) + (form.ClientSize.Width / 2) - (label.Size.Width / 2);
+            int LocationY = (form.ClientSize.Height / 2) - (label.Size.Height / 2);
+
+            label.Location = new Point(LocationX, LocationY);
+            label.Anchor = AnchorStyles.None;
+
+            return label;
+        }
+        public static Button CreateButton(Color backColor, Color foreColor)
+        {
+            Button button = new Button();
+            button.Text = "\u2630";
+            button.TextAlign = ContentAlignment.MiddleCenter;
+            button.BackColor = backColor;
+            button.ForeColor = foreColor;
+            button.Font = new Font("Arial", 30, FontStyle.Bold);
+            button.Size = new Size(55, 55);
+            button.Location = new Point(15, 15);
+            button.FlatStyle = FlatStyle.Flat;
+            button.FlatAppearance.BorderColor = backColor; ;
+            button.FlatAppearance.MouseDownBackColor = Color.FromArgb(80, 208, 200, 178);
+            button.FlatAppearance.MouseOverBackColor = Color.FromArgb(80, 116, 108, 97);
+            return button;
+        }
+        public static Button CreateButtonMenu(Color backColor, Color foreColor, string text)
+        {
+            Button button = new Button();
+            button.Text = text;
+            button.TextAlign = ContentAlignment.MiddleCenter;
+            button.BackColor = backColor;
+            button.ForeColor = foreColor;
+            button.Font = new Font("Arial", 30, FontStyle.Bold);
+            button.Size = new Size(100, 75);
+            button.Location = new Point(15, 15);
+            button.FlatStyle = FlatStyle.Flat;
+            button.FlatAppearance.BorderColor = backColor; ;
+            button.FlatAppearance.MouseDownBackColor = Color.FromArgb(80, 208, 200, 178);
+            button.FlatAppearance.MouseOverBackColor = Color.FromArgb(80, 116, 108, 97);
+            return button;
+        }
+        public static Button CreateButtonAdd(Color backColor, Color foreColor, string text)
+        {
+            Button button = new Button();
+            button.Text = text;
+            button.TextAlign = ContentAlignment.MiddleCenter;
+            button.BackColor = backColor;
+            button.ForeColor = foreColor;
+            button.Font = new Font("Arial", 10, FontStyle.Bold);
+            button.Size = new Size(100, 30);
+            button.Location = new Point(150, 650);
+            button.FlatStyle = FlatStyle.Flat;
+            button.FlatAppearance.BorderColor = Color.Black;
+            button.FlatAppearance.MouseDownBackColor = Color.FromArgb(80, 208, 200, 178);
+            button.FlatAppearance.MouseOverBackColor = Color.FromArgb(80, 116, 108, 97);
+            return button;
+        }
+        public static string GetDate()
+        {
+            DateTime now = DateTime.Now;
+            string dayOfWeek = now.ToString("dddd", new CultureInfo("fr-FR"));
+            string dayOfMonth = now.ToString("dd");
+            string month = now.ToString("MMMM", new CultureInfo("fr-FR"));
+            return $"{dayOfWeek} {dayOfMonth} {month}";
+
+        }
+
+    }
+    public static class ScreenSetings
+    {
+        public static int GetWidthScreen(Form form)
+        {
+            return form.ClientSize.Width;
+        }
+        public static int GetHeightScreen(Form form)
+        {
+            return form.ClientSize.Height;
+        }
+
+    }
+    public class Personnes
+    {
+        public string name;
+        public string firstName;
+        public bool genre;
+
+        public Personnes(string name, string firstName, bool genre)
+        {
+            this.name = name;
+            this.firstName = firstName;
+            this.genre = genre;
+        }
+        public string AfficherNames()
+        {
+            return name.ToUpper() + " " + firstName.Substring(0, 1).ToUpper() + firstName.Substring(1);
+        }
+    }
+    class GetListPersonnes
+    {
+        public static List<Personnes> GetListPersonneFromJson()
+        {
+            string json = ImportJsonFromFile.GetJsonFromFile();
+
+            List<Personnes> Personnes = JsonConvert.DeserializeObject<List<Personnes>>(json);
+            return Personnes;
+        }
+    }
+}
+
