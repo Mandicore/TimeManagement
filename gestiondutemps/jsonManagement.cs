eb3a10cb2f8b33235de482887a353242795b6117
FIGUEIRAS Jossua <105790280+Mandicore@users.noreply.github.com>
2023-05-21 11:09:12 +0200

    Add files via upload
    change file name

---
+++ gestiondutemps/jsonManagement.cs
@@ -0,0 +1,55 @@
+﻿using Gestion_du_temps_cse_axe_system_.net_5._0;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+using System.IO;
+using System.Windows.Forms;
+
+namespace projet_gestion_temps_cse_axe_system
+{
+    internal class jsonManagement
+    {
+    }
+    public class ImportJsonFromFile
+    {
+        public static string GetJsonFromFile()
+        {
+            string json = "";
+            string file = "Data";
+            string filePath = "Data/personneList.json";
+
+            if (!Directory.Exists(file))
+            {
+                Directory.CreateDirectory(file);
+                using (StreamWriter sw = File.CreateText(filePath))
+                {
+                    sw.Write(json);
+                }
+            }
+            return File.ReadAllText("Data/personneList.json");
+        }
+        public static void Send(List<Personnes> personnes)
+        {
+            string json = JsonConvert.SerializeObject(personnes);
+            string file = "Data";
+            string filePath = "Data/personneList.json";
+
+            if (File.Exists(filePath))
+            {
+                File.WriteAllText(filePath, json);
+            }
+            else
+            {
+                if (!Directory.Exists(file))
+                {
+                    Directory.CreateDirectory(file);
+                }
+                using (StreamWriter sw = File.CreateText(filePath))
+                {
+                    sw.Write(json);
+                }
+            }
+        }
+    }
+}
